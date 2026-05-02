7e3d3309feb9e72e5a059bde80af17b466ccafa5
Mateusz Pomianek <matpomianek@gmail.com>
2026-05-02 12:34:28 +0200

    loc2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/releases/") }
     }
 }
 
